dfe2f7b07f41164e1ed74b3ad32e9aca9420e2ae
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-05-25 23:42:53 +0900

    first commit

--- RSP.java
+++ RSP.java
@@ -3,73 +3,78 @@ import java.util.Scanner;
 
 public class RSP {
 	public static void main(String[] args) {
-		//プレイヤーの手を入力
-		System.out.println("あなたの手を入力してください．0:グー 1:チョキ 2: パー");
+		// テキストに使う色の宣言
+		final String GREEN = "\u001b[00;32m";
+		final String YELLOW = "\u001b[00;33m";
+		final String PURPLE = "\u001b[00;34m";
+
+		// プレイヤーの手を入力
+		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
 		Scanner scan = new Scanner(System.in);
-        int player_hand = Integer.parseInt(scan.nextLine());
-        scan.close();
-        System.out.println("あなたの手は" + player_hand + "です");
-        
-        //相手の手を決定
-        int enemy_hand = getRandomInt(2);
-        System.out.println("相手の手は" + enemy_hand + "です");
-        
-        //勝敗判定
-        switch(player_hand) {
-        	case 0:
-        		switch(enemy_hand) {
-	        		case 0:
-	        			System.out.println("あいこです");
-	        			break;
-	        		case 1:
-	        			System.out.println("あなたの勝ちです");
-	        			break;
-	        		case 2:
-	        			System.out.println("相手の勝ちです");
-	        			break;
-	        		default:
-	        			break;
-        		}
-        		break;
-        	case 1:
-        		switch(enemy_hand) {
-        		case 0:
-        			System.out.println("あいこです");
-        			break;
-        		case 1:
-        			System.out.println("相手の勝ちです");
-        			break;
-        		case 2:
-        			System.out.println("あなたの勝ちです");
-        			break;
-        		default:
-        			break;
-	    		}
-	    		break;
-        	case 2:
-        		switch(enemy_hand) {
-        		case 0:
-        			System.out.println("あなたの勝ちです");
-        			break;
-        		case 1:
-        			System.out.println("相手の勝ちです");
-        			break;
-        		case 2:
-        			System.out.println("あいこです");
-        			break;
-        		default:
-        			break;
-	    		}
-	    		break;
-	    	default:
-	    		break;
-        }
+		int player_hand = Integer.parseInt(scan.nextLine());
+		scan.close();
+
+		// 相手の手を決定
+		int enemy_hand = getRandomInt(2);
+		System.out.println(" Enemy hand is: " + enemy_hand);
+
+		System.out.print("Result: ");
+		// 勝敗判定
+		switch (player_hand) {
+			case 0:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(GREEN + "Draw!");
+						break;
+					case 1:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 2:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					default:
+						break;
+				}
+				break;
+			case 1:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(GREEN + "Draw!");
+						break;
+					case 1:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 2:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					default:
+						break;
+				}
+				break;
+			case 2:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 1:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					case 2:
+						System.out.println(GREEN + "Draw!");
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;
+		}
 	}
-	
-	//受け取った範囲でランダムな数値を生成
+
+	// 受け取った範囲でランダムな数値を生成
 	private static int getRandomInt(int range) {
 		Random random = new Random();
-        int random_num = random.nextInt(range);
+		int random_num = random.nextInt(range);
 		return random_num;
 	}
 }
