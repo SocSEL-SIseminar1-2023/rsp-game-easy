94361ddba116d1b3c60b15dc1ecbc8e7760f6150
中川英哉 <134668207+NakagawaHideya@users.noreply.github.com>
2023-05-30 16:03:30 +0900

    0〜2以外の入力に対するエラー処理をメソッド化した
    pull requests #48をメソッド化した

--- RSP.java
+++ RSP.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class RSP {
 	public static void main(String[] args) {
@@ -7,14 +8,14 @@ public class RSP {
 		final String GREEN = "\u001b[00;32m";
 		final String YELLOW = "\u001b[00;33m";
 		final String PURPLE = "\u001b[00;34m";
+        int player_hand = 0;
+
 		// プレイヤーの手を入力
-		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
-		Scanner scan = new Scanner(System.in);
-		int player_hand = Integer.parseInt(scan.nextLine());
-		scan.close();
+        player_hand = getPlayerHand();
+        
 
 		// 相手の手を決定
-		int enemy_hand = getRandomInt(2);
+	    int enemy_hand = getRandomInt(2);
 		System.out.println(" Enemy hand is: " + enemy_hand);
 
 		System.out.print("Result: ");
@@ -37,13 +38,13 @@ public class RSP {
 				break;
 			case 1:
 				switch (enemy_hand) {
-					case 0:
+					case 1:
 						System.out.println(GREEN + "Draw!");
 						break;
-					case 1:
+					case 2:
 						System.out.println(YELLOW + "You win!");
 						break;
-					case 2:
+					case 0:
 						System.out.println(PURPLE + "Enemy win!");
 						break;
 					default:
@@ -76,4 +77,27 @@ public class RSP {
 		int random_num = random.nextInt(range);
 		return random_num;
 	}
+
+    //プレイヤーの手入力するメソッド
+    private static int getPlayerHand() {
+        int player_hand = 0;
+        boolean flag = false; // 入力が正しいかどうか判断するフラグ
+        Scanner scan = new Scanner(System.in);
+        while (!flag) {
+            try {
+                System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\nYour hand is : ");
+                player_hand = scan.nextInt();
+                if (player_hand >= 0 && player_hand <= 2) {
+                    flag = true;
+                } else {
+                    System.out.println("Error: Please enter a number between 0 and 2");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Please enter a number between 0 and 2");
+                scan.nextLine(); // 入力バッファをクリア
+            }
+        }
+        scan.close();
+        return player_hand;
+    }
 }
