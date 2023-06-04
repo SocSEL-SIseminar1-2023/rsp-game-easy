8dd4b7a6920c8f8fef31cf1266748561afe3ab14
中川英哉 <134668207+NakagawaHideya@users.noreply.github.com>
2023-06-05 00:18:31 +0900

    機能追加とメソッド化による可読性の向上
    ・追加機能
    あいこの時の再戦機能を追加
    
    ・メソッド化
    勝敗判定をメソッド化し可読性を向上させた。

--- RSP.java
+++ RSP.java
@@ -1,79 +1,123 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class RSP {
-	public static void main(String[] args) {
-		// テキストに使う色の宣言
-		final String GREEN = "\u001b[00;32m";
-		final String YELLOW = "\u001b[00;33m";
-		final String PURPLE = "\u001b[00;34m";
-		// プレイヤーの手を入力
-		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
-		Scanner scan = new Scanner(System.in);
-		int player_hand = Integer.parseInt(scan.nextLine());
-		scan.close();
+    public static void main(String[] args) {
+        // テキストに使う色の宣言
+        final String GREEN = "\u001b[00;32m";
+        final String YELLOW = "\u001b[00;33m";
+        final String PURPLE = "\u001b[00;34m";
+        final String RESET = "\u001b[0m";
 
-		// 相手の手を決定
-		int enemy_hand = getRandomInt(2);
-		System.out.println(" Enemy hand is: " + enemy_hand);
+        // プレイヤーの手を入力
+        int player_hand = getPlayerHand();
 
-		System.out.print("Result: ");
-		// 勝敗判定
-		switch (player_hand) {
-			case 0:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 1:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 2:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 1:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					case 2:
-						System.out.println(GREEN + "Draw!");
-						break;
-					default:
-						break;
-				}
-				break;
-			default:
-				break;
-		}
-	}
+        // 勝敗判定のループ
+        boolean rematch = true;
+        while (rematch) {
+            // 相手の手を決定
+            int enemy_hand = getRandomInt(2);
+            System.out.println("Enemy hand is: " + enemy_hand);
 
-	// 受け取った範囲でランダムな数値を生成
-	private static int getRandomInt(int range) {
-		Random random = new Random();
-		int random_num = random.nextInt(range);
-		return random_num;
-	}
+            System.out.print("Result: ");
+            playRockPaperScissors(player_hand, enemy_hand, GREEN, YELLOW, PURPLE);
+
+            // あいこの場合、もう一度手を入力するか確認
+            if (player_hand == enemy_hand) {
+                System.out.println(RESET + "Rematch!");
+                player_hand = getPlayerHand();
+            } else {
+                rematch = false;
+            }
+        }
+    }
+
+    // 受け取った範囲でランダムな数値を生成
+    private static int getRandomInt(int range) {
+        Random random = new Random();
+        int random_num = random.nextInt(range);
+        return random_num;
+    }
+
+    // プレイヤーの手を入力するメソッド
+    private static int getPlayerHand() {
+        int player_hand = 0;
+        boolean flag = false; // 入力が正しいかどうか判断するフラグ
+        Scanner scan = new Scanner(System.in);
+        while (!flag) {
+            try {
+                System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\nYour hand is : ");
+                player_hand = scan.nextInt();
+                if (player_hand >= 0 && player_hand <= 2) {
+                    flag = true;
+                } else {
+                    System.out.println("Error: Please enter a number between 0 and 2");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Please enter a number between 0 and 2");
+                scan.nextLine(); // 入力バッファをクリア
+            }
+        }
+        return player_hand;
+    }
+
+    // じゃんけんの結果を表示するメソッド
+    private static void playRockPaperScissors(int playerHand, int enemyHand, String green, String yellow, String purple) {
+        final String GREEN = green;
+        final String YELLOW = yellow;
+        final String PURPLE = purple;
+        final String RESET = "\u001b[0m";
+
+        switch (playerHand) {
+            case 0:
+                switch (enemyHand) {
+                    case 0:
+                        System.out.println(GREEN + "Draw!");
+                        break;
+                    case 1:
+                        System.out.println(YELLOW + "You win!");
+                        break;
+                    case 2:
+                        System.out.println(PURPLE + "Enemy win!");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 1:
+                switch (enemyHand) {
+                    case 1:
+                        System.out.println(GREEN + "Draw!");
+                        break;
+                    case 2:
+                        System.out.println(YELLOW + "You win!");
+                        break;
+                    case 0:
+                        System.out.println(PURPLE + "Enemy win!");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                switch (enemyHand) {
+                    case 0:
+                        System.out.println(YELLOW + "You win!");
+                        break;
+                    case 1:
+                        System.out.println(PURPLE + "Enemy win!");
+                        break;
+                    case 2:
+                        System.out.println(GREEN + "Draw!");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        System.out.print(RESET);
+    }
 }
